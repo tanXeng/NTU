7fbad6f9586974a4150b7b213b4ec2c5ae283f3d
tanXeng <tanxengian@gmail.com>
2024-09-12 11:04:18 +0800

    Update battle_ship.py

--- battle_ship.py
+++ battle_ship.py
@@ -1,8 +1,49 @@
+board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         ]
+
+def show_board():
+    for i in board:
+        for j in i:
+            print(j, end=" ")
+        print("\n")
+
+def place_ship(y, x, type, ori):
+    if ori == "v":
+        if type == "c":
+            board[-y - 1][x] = 1
+            board[-y    ][x] = 1
+            board[-y + 1][x] = 1
+            board[-y + 2][x] = 1
+        else:
+            board[-y - 1][x] = 1
+            board[-y - 2][x] = 1
+            board[-y - 3][x] = 1
+    else:
+        if type == "c":
+            board[-y - 1][x] = 1
+            board[-y - 1][x + 1] = 1
+            board[-y - 1][x + 2] = 1
+            board[-y - 1][x + 3] = 1
+        else:
+            board[-y - 1][x] = 1
+            board[-y - 1][x + 1] = 1
+            board[-y - 1][x + 2] = 1
+
 while True:
-    ship_type = str(input("Submarine or carrier (c/s): "))
-    if ship_type == "quit":
+    ship_type = input("Submarine or carrier (c/s): ")
+    if str(ship_type) == "quit":
         break
-    elif ship_type not in "cs":
+    elif str(ship_type) not in "cs":
         print("enter something valid!")
         continue
 
@@ -20,12 +61,12 @@ while True:
         print("enter something valid!")
         continue
 
-    ship_dept = input("ship orientation vertical or horizontal (v/h) ")
+    ship_ori = input("ship orientation vertical or horizontal (v/h) ")
     if ship_type == "quit":
         break
-    elif ship_dept not in "vh":
+    elif ship_ori not in "vh":
         print("enter something valid!")
         continue
 
-    ship = (ship_type ,int(ship_pos_x), int(ship_pos_y), ship_dept)
-    print(ship)
+    place_ship(int(ship_pos_y), int(ship_pos_x), ship_type, ship_ori)
+    show_board()
